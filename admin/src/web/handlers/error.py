b87b7a9b9f8f641711b19ccf942b2af1c8172b5d
Joaquín Santa Cruz <joacosanta@gmail.com>
2024-09-13 19:13:12 -0300

    Correcciones template_error

--- admin/src/web/handlers/error.py
+++ admin/src/web/handlers/error.py
@@ -1,12 +1,11 @@
-from dataclasses import dataclass
 from flask import render_template
 
-@dataclass
-class Error:
-    code: int
-    message: str
-    description: str
+error = {
+    "title": "Ups, algo salió mal...",
+    "code": 404,
+    "message": "Not Found",
+    "description": "La URL solicitada no se encontró en el servidor."
+}
 
 def error_not_found(e):
-    error = Error(404, "Not Found", "La URL solicitada no se encontró en el servidor.")
     return render_template("error.html", error = error), 404
