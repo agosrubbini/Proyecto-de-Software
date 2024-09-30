37e23af4eb2dd11249dad135f6d0cbd2842924b0
Joaquina Saadi <joaquinasaadi@gmail.com>
2024-09-30 17:50:39 -0300

    comienzo de funcionalidad bloqueo de usuarios

--- admin/src/web/controllers/users.py
+++ admin/src/web/controllers/users.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import render_template
 from flask import url_for
 from src.core.auth.models.user import User
+from src.core.auth.models.roles_and_permissions import Role
 
 bp = Blueprint('users', __name__, url_prefix='/users')
 
@@ -9,7 +10,9 @@ bp = Blueprint('users', __name__, url_prefix='/users')
 def list_users():
     users = None
     users = User.query.all()
+    roles = Role.query.all()
     context = {
-        'users': users
+        'users': users,
+        'roles': roles
     }
     return render_template('users.html', context=context)
