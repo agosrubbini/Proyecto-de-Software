debc0e25ebfd7085fd196c2a502e8218ca101a80
Joaquín Santa Cruz <joacosanta@gmail.com>
2024-10-04 15:45:34 -0300

    Agrego permisos y rol_permiso a la db

--- admin/src/core/auth/__init__.py
+++ admin/src/core/auth/__init__.py
@@ -65,3 +65,56 @@ def find_role_id_by_name(name):
     """Devuelve el id del rol con el nombre pasado por parámetro"""
 
     return Role.query.filter_by(name=name).first().id
+
+
+def initialice_admin_permissions():
+    admin_role = Role.query.filter_by(name="Administración").first()
+
+    # Lista de permisos para el rol Administración
+    admin_permissions = [
+        "team_index", "team_new", "team_show", "team_update", "team_destroy",
+        "payment_index", "payment_new", "payment_show", "payment_update", "payment_destroy",
+        "jya_index", "jya_new", "jya_show", "jya_update", "jya_destroy",
+        "billing_index", "billing_new", "billing_show", "billing_update", "billing_destroy",
+        "horse_index", "horse_show"
+    ]
+
+    for perm_name in admin_permissions:
+        permission = Permision.query.filter_by(name=perm_name).first()
+        if permission:
+            admin_role.permissions.append(permission)
+
+    db.session.commit()
+
+def initialice_tecnica_permissions():
+    tecnica_role = Role.query.filter_by(name="Técnica").first()
+
+    # Lista de permisos para el rol Técnica
+    tecnica_permissions = [
+        "jya_index", "jya_new", "jya_show", "jya_update", "jya_destroy",
+        "billing_index", "billing_show",
+        "horse_index", "horse_show"
+    ]
+
+    for perm_name in tecnica_permissions:
+        permission = Permision.query.filter_by(name=perm_name).first()
+        if permission:
+            tecnica_role.permissions.append(permission)
+
+    db.session.commit()
+
+def initialice_ecuestre_permissions():
+    ecuestre_role = Role.query.filter_by(name="Ecuestre").first()
+
+    # Lista de permisos para el rol Ecuestre
+    ecuestre_permissions = [
+        "jya_index", "jya_show",
+        "horse_index", "horse_new", "horse_show", "horse_update", "horse_destroy"
+    ]
+
+    for perm_name in ecuestre_permissions:
+        permission = Permision.query.filter_by(name=perm_name).first()
+        if permission:
+            ecuestre_role.permissions.append(permission)
+
+    db.session.commit()
