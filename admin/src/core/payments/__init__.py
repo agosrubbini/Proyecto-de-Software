7e9de887ac7b6f374a57db4176c1ac473294eb83
Joaquina Saadi <joaquinasaadi@gmail.com>
2024-10-25 21:10:07 -0300

    validation payment

--- admin/src/core/payments/__init__.py
+++ admin/src/core/payments/__init__.py
@@ -16,8 +16,8 @@ def create_billing(**kwargs):
 
     return billing
 
-def validate_beneficiary(beneficiary):
+def validate_beneficiary(beneficiary_id):
     '''
         Valida que el beneficiario ingresado sea un usuario registrado
     '''
-    return Payment.query.filter_by(beneficiary=beneficiary).first()
+    return Payment.query.filter_by(id=beneficiary_id).first()
