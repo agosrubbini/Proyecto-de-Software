c7b635a838a1dc7121c6261fe015ce076bd4c2ea
Agostina Rubbini <agostinarubbini@gmail.com>
2024-11-17 16:32:45 -0300

    pruebo google

--- admin/src/web/routes.py
+++ admin/src/web/routes.py
@@ -2,12 +2,18 @@ from flask import redirect, render_template, url_for
 from src.core.auth.models.user import User
 from src.core.auth.auth import inject_user_permissions
 from authlib.integrations.flask_client import OAuth
+from flask import send_from_directory
+import os
 
 def register(app):
     @app.route("/")
     @inject_user_permissions
     def home():
         return render_template("home.html")
+    
+    @app.route('/')
+    def google():
+        return send_from_directory(os.path.join(app.root_path, 'static'), 'googlefc8a906de75f1f28.html')
     
     @app.route('/login')
     def login():
