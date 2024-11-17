5f494abb50847abf45bfb38117885c8cf7de20f0
Agostina Rubbini <agostinarubbini@gmail.com>
2024-11-17 16:26:30 -0300

    pruebo google

--- admin/app.py
+++ admin/app.py
@@ -1,6 +1,19 @@
 from src.web import create_app
+from authlib.integrations.flask_client import OAuth
 
 app = create_app()
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id='',
+    client_secret='',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1',
+    client_kwargs={'scope': 'openid profile email'}
+)
 
 if __name__ == "__main__":
     app.run()
--- admin/src/web/routes.py
+++ admin/src/web/routes.py
@@ -1,9 +1,26 @@
-from flask import render_template
+from flask import redirect, render_template, url_for
 from src.core.auth.models.user import User
 from src.core.auth.auth import inject_user_permissions
+from authlib.integrations.flask_client import OAuth
 
 def register(app):
     @app.route("/")
     @inject_user_permissions
     def home():
         return render_template("home.html")
+    
+    @app.route('/login')
+    def login():
+        google = OAuth.create_client('google')
+        redirect_uri = url_for('authorize', _external=True)
+        return google.authorize_redirect(redirect_uri)
+
+    @app.route('/authorize')
+    def authorize():
+        google = OAuth.create_client('google')
+        token = google.authorize_access_token()
+        resp = google.get(userinfo)
+        userinfo = resp.json()
+        #profile = resp.json()
+        # do something with the token and profile
+        return redirect('/')
