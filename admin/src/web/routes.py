790d047c18bdbb3de8da94e271aa18ef0522047b
Agostina Rubbini <agostinarubbini@gmail.com>
2024-11-17 16:44:28 -0300

    pruebo google

--- admin/src/web/routes.py
+++ admin/src/web/routes.py
@@ -13,8 +13,9 @@ def register(app):
     
     @app.route('/googlefc8a906de75f1f28.html')
     def google():
+    # Servir el archivo desde la carpeta estática pero sin /static en la URL
         return send_from_directory(os.path.join(app.root_path, 'static'), 'googlefc8a906de75f1f28.html')
-    
+
     @app.route('/login')
     def login():
         google = OAuth.create_client('google')
